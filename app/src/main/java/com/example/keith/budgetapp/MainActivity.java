18eb96415211b935a3f47505e177478d3b7eb940
kmont12 <kmonteiro1222@gmail.com>
2016-10-10 21:31:10 -0400

    main page modifications

--- app/src/main/java/com/example/keith/budgetapp/MainActivity.java
+++ app/src/main/java/com/example/keith/budgetapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.keith.budgetapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void purchaseLogger(View view) {
+    }
+
+    public void balanceChecker(View view) {
     }
 }
